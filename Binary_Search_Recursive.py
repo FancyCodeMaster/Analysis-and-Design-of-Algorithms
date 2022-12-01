95409fb69603c623206dd355df91a61d220fd5c0
FancyCodeMaster <fancycodemaster@gmail.com>
2022-12-01 23:31:52 +0545

    Binary Search Recursive

--- Binary_Search_Recursive.py
+++ Binary_Search_Recursive.py
@@ -7,14 +7,12 @@ def binary_search(arr , low , high , element):
         return low
     else:
         mid = math.floor((low+high) / 2)
-        print("mid is" , mid)
         if arr[mid] > element:
             binary_search(arr , low , mid-1 , element)
         elif arr[mid] == element:
             return mid
         else:
             binary_search(arr , mid+1 , high , element)
-        print("low is" , low , "high is" , high)
 
 arr_to_search = [1 , 2 , 100 , 500 , 699 , 789 , 897]
 
